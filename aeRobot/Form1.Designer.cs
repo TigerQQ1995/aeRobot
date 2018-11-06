35a17989c1b6cf895b8abc9c237e41024942f1fd
r <r@DESKTOP-GPIUP35>
2018-11-06 09:10:59 +0800

    添加项目文件。

---
+++ aeRobot/Form1.Designer.cs
@@ -0,0 +1,412 @@
+﻿namespace aeRobot
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.J1 = new System.Windows.Forms.Label();
+            this.J2 = new System.Windows.Forms.Label();
+            this.J3 = new System.Windows.Forms.Label();
+            this.J4 = new System.Windows.Forms.Label();
+            this.J5 = new System.Windows.Forms.Label();
+            this.J6 = new System.Windows.Forms.Label();
+            this.J1UP = new System.Windows.Forms.Button();
+            this.J1DOWN = new System.Windows.Forms.Button();
+            this.J2DOWN = new System.Windows.Forms.Button();
+            this.J2UP = new System.Windows.Forms.Button();
+            this.J3DOWN = new System.Windows.Forms.Button();
+            this.J3UP = new System.Windows.Forms.Button();
+            this.J4DOWN = new System.Windows.Forms.Button();
+            this.J4UP = new System.Windows.Forms.Button();
+            this.J5DOWN = new System.Windows.Forms.Button();
+            this.J5UP = new System.Windows.Forms.Button();
+            this.J6DOWN = new System.Windows.Forms.Button();
+            this.J6UP = new System.Windows.Forms.Button();
+            this.J1BOX = new System.Windows.Forms.TextBox();
+            this.J2BOX = new System.Windows.Forms.TextBox();
+            this.J3BOX = new System.Windows.Forms.TextBox();
+            this.J4BOX = new System.Windows.Forms.TextBox();
+            this.J5BOX = new System.Windows.Forms.TextBox();
+            this.J6BOX = new System.Windows.Forms.TextBox();
+            this.vs = new System.Windows.Forms.TextBox();
+            this.vdown = new System.Windows.Forms.Button();
+            this.vup = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pause = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // J1
+            // 
+            this.J1.AutoSize = true;
+            this.J1.Location = new System.Drawing.Point(490, 49);
+            this.J1.Name = "J1";
+            this.J1.Size = new System.Drawing.Size(31, 15);
+            this.J1.TabIndex = 0;
+            this.J1.Text = "J1:";
+            // 
+            // J2
+            // 
+            this.J2.AutoSize = true;
+            this.J2.Location = new System.Drawing.Point(490, 102);
+            this.J2.Name = "J2";
+            this.J2.Size = new System.Drawing.Size(31, 15);
+            this.J2.TabIndex = 1;
+            this.J2.Text = "J2:";
+            // 
+            // J3
+            // 
+            this.J3.AutoSize = true;
+            this.J3.Location = new System.Drawing.Point(490, 162);
+            this.J3.Name = "J3";
+            this.J3.Size = new System.Drawing.Size(31, 15);
+            this.J3.TabIndex = 2;
+            this.J3.Text = "J3:";
+            // 
+            // J4
+            // 
+            this.J4.AutoSize = true;
+            this.J4.Location = new System.Drawing.Point(490, 222);
+            this.J4.Name = "J4";
+            this.J4.Size = new System.Drawing.Size(31, 15);
+            this.J4.TabIndex = 3;
+            this.J4.Text = "J4:";
+            // 
+            // J5
+            // 
+            this.J5.AutoSize = true;
+            this.J5.Location = new System.Drawing.Point(490, 282);
+            this.J5.Name = "J5";
+            this.J5.Size = new System.Drawing.Size(31, 15);
+            this.J5.TabIndex = 4;
+            this.J5.Text = "J5:";
+            // 
+            // J6
+            // 
+            this.J6.AutoSize = true;
+            this.J6.Location = new System.Drawing.Point(490, 355);
+            this.J6.Name = "J6";
+            this.J6.Size = new System.Drawing.Size(31, 15);
+            this.J6.TabIndex = 5;
+            this.J6.Text = "J6:";
+            // 
+            // J1UP
+            // 
+            this.J1UP.Location = new System.Drawing.Point(527, 41);
+            this.J1UP.Name = "J1UP";
+            this.J1UP.Size = new System.Drawing.Size(79, 30);
+            this.J1UP.TabIndex = 6;
+            this.J1UP.Text = "+";
+            this.J1UP.UseVisualStyleBackColor = true;
+            this.J1UP.Click += new System.EventHandler(this.J1UP_Click);
+            // 
+            // J1DOWN
+            // 
+            this.J1DOWN.Location = new System.Drawing.Point(811, 41);
+            this.J1DOWN.Name = "J1DOWN";
+            this.J1DOWN.Size = new System.Drawing.Size(79, 30);
+            this.J1DOWN.TabIndex = 7;
+            this.J1DOWN.Text = "-";
+            this.J1DOWN.UseVisualStyleBackColor = true;
+            this.J1DOWN.Click += new System.EventHandler(this.J1DOWN_Click);
+            // 
+            // J2DOWN
+            // 
+            this.J2DOWN.Location = new System.Drawing.Point(811, 102);
+            this.J2DOWN.Name = "J2DOWN";
+            this.J2DOWN.Size = new System.Drawing.Size(79, 30);
+            this.J2DOWN.TabIndex = 9;
+            this.J2DOWN.Text = "-";
+            this.J2DOWN.UseVisualStyleBackColor = true;
+            this.J2DOWN.Click += new System.EventHandler(this.J2DOWN_Click);
+            // 
+            // J2UP
+            // 
+            this.J2UP.Location = new System.Drawing.Point(527, 102);
+            this.J2UP.Name = "J2UP";
+            this.J2UP.Size = new System.Drawing.Size(79, 30);
+            this.J2UP.TabIndex = 8;
+            this.J2UP.Text = "+";
+            this.J2UP.UseVisualStyleBackColor = true;
+            this.J2UP.Click += new System.EventHandler(this.J2UP_Click);
+            // 
+            // J3DOWN
+            // 
+            this.J3DOWN.Location = new System.Drawing.Point(811, 162);
+            this.J3DOWN.Name = "J3DOWN";
+            this.J3DOWN.Size = new System.Drawing.Size(79, 30);
+            this.J3DOWN.TabIndex = 11;
+            this.J3DOWN.Text = "-";
+            this.J3DOWN.UseVisualStyleBackColor = true;
+            this.J3DOWN.Click += new System.EventHandler(this.J3DOWN_Click);
+            // 
+            // J3UP
+            // 
+            this.J3UP.Location = new System.Drawing.Point(527, 162);
+            this.J3UP.Name = "J3UP";
+            this.J3UP.Size = new System.Drawing.Size(79, 30);
+            this.J3UP.TabIndex = 10;
+            this.J3UP.Text = "+";
+            this.J3UP.UseVisualStyleBackColor = true;
+            this.J3UP.Click += new System.EventHandler(this.J3UP_Click);
+            // 
+            // J4DOWN
+            // 
+            this.J4DOWN.Location = new System.Drawing.Point(811, 222);
+            this.J4DOWN.Name = "J4DOWN";
+            this.J4DOWN.Size = new System.Drawing.Size(79, 30);
+            this.J4DOWN.TabIndex = 13;
+            this.J4DOWN.Text = "-";
+            this.J4DOWN.UseVisualStyleBackColor = true;
+            this.J4DOWN.Click += new System.EventHandler(this.J4DOWN_Click);
+            // 
+            // J4UP
+            // 
+            this.J4UP.Location = new System.Drawing.Point(527, 222);
+            this.J4UP.Name = "J4UP";
+            this.J4UP.Size = new System.Drawing.Size(79, 30);
+            this.J4UP.TabIndex = 12;
+            this.J4UP.Text = "+";
+            this.J4UP.UseVisualStyleBackColor = true;
+            this.J4UP.Click += new System.EventHandler(this.J4UP_Click);
+            // 
+            // J5DOWN
+            // 
+            this.J5DOWN.Location = new System.Drawing.Point(811, 282);
+            this.J5DOWN.Name = "J5DOWN";
+            this.J5DOWN.Size = new System.Drawing.Size(79, 30);
+            this.J5DOWN.TabIndex = 15;
+            this.J5DOWN.Text = "-";
+            this.J5DOWN.UseVisualStyleBackColor = true;
+            this.J5DOWN.Click += new System.EventHandler(this.J5DOWN_Click);
+            // 
+            // J5UP
+            // 
+            this.J5UP.Location = new System.Drawing.Point(527, 282);
+            this.J5UP.Name = "J5UP";
+            this.J5UP.Size = new System.Drawing.Size(79, 30);
+            this.J5UP.TabIndex = 14;
+            this.J5UP.Text = "+";
+            this.J5UP.UseVisualStyleBackColor = true;
+            this.J5UP.Click += new System.EventHandler(this.J5UP_Click);
+            // 
+            // J6DOWN
+            // 
+            this.J6DOWN.Location = new System.Drawing.Point(811, 347);
+            this.J6DOWN.Name = "J6DOWN";
+            this.J6DOWN.Size = new System.Drawing.Size(79, 30);
+            this.J6DOWN.TabIndex = 17;
+            this.J6DOWN.Text = "-";
+            this.J6DOWN.UseVisualStyleBackColor = true;
+            this.J6DOWN.Click += new System.EventHandler(this.J6DOWN_Click);
+            // 
+            // J6UP
+            // 
+            this.J6UP.Location = new System.Drawing.Point(527, 347);
+            this.J6UP.Name = "J6UP";
+            this.J6UP.Size = new System.Drawing.Size(79, 30);
+            this.J6UP.TabIndex = 16;
+            this.J6UP.Text = "+";
+            this.J6UP.UseVisualStyleBackColor = true;
+            this.J6UP.Click += new System.EventHandler(this.J6UP_Click);
+            // 
+            // J1BOX
+            // 
+            this.J1BOX.Location = new System.Drawing.Point(612, 41);
+            this.J1BOX.Multiline = true;
+            this.J1BOX.Name = "J1BOX";
+            this.J1BOX.Size = new System.Drawing.Size(183, 30);
+            this.J1BOX.TabIndex = 18;
+            this.J1BOX.Text = "0.00000000";
+            // 
+            // J2BOX
+            // 
+            this.J2BOX.Location = new System.Drawing.Point(612, 102);
+            this.J2BOX.Multiline = true;
+            this.J2BOX.Name = "J2BOX";
+            this.J2BOX.Size = new System.Drawing.Size(183, 30);
+            this.J2BOX.TabIndex = 19;
+            this.J2BOX.Text = "0.00000000";
+            // 
+            // J3BOX
+            // 
+            this.J3BOX.Location = new System.Drawing.Point(612, 167);
+            this.J3BOX.Multiline = true;
+            this.J3BOX.Name = "J3BOX";
+            this.J3BOX.Size = new System.Drawing.Size(183, 30);
+            this.J3BOX.TabIndex = 20;
+            this.J3BOX.Text = "0.00000000";
+            // 
+            // J4BOX
+            // 
+            this.J4BOX.Location = new System.Drawing.Point(612, 227);
+            this.J4BOX.Multiline = true;
+            this.J4BOX.Name = "J4BOX";
+            this.J4BOX.Size = new System.Drawing.Size(183, 30);
+            this.J4BOX.TabIndex = 21;
+            this.J4BOX.Text = "0.00000000";
+            // 
+            // J5BOX
+            // 
+            this.J5BOX.Location = new System.Drawing.Point(612, 279);
+            this.J5BOX.Multiline = true;
+            this.J5BOX.Name = "J5BOX";
+            this.J5BOX.Size = new System.Drawing.Size(183, 30);
+            this.J5BOX.TabIndex = 22;
+            this.J5BOX.Text = "0.00000000";
+            // 
+            // J6BOX
+            // 
+            this.J6BOX.Location = new System.Drawing.Point(612, 347);
+            this.J6BOX.Multiline = true;
+            this.J6BOX.Name = "J6BOX";
+            this.J6BOX.Size = new System.Drawing.Size(183, 30);
+            this.J6BOX.TabIndex = 23;
+            this.J6BOX.Text = "0.00000000";
+            // 
+            // vs
+            // 
+            this.vs.Location = new System.Drawing.Point(612, 406);
+            this.vs.Multiline = true;
+            this.vs.Name = "vs";
+            this.vs.Size = new System.Drawing.Size(183, 30);
+            this.vs.TabIndex = 27;
+            this.vs.Text = "5.00000000";
+            // 
+            // vdown
+            // 
+            this.vdown.Location = new System.Drawing.Point(811, 406);
+            this.vdown.Name = "vdown";
+            this.vdown.Size = new System.Drawing.Size(79, 30);
+            this.vdown.TabIndex = 26;
+            this.vdown.Text = "-";
+            this.vdown.UseVisualStyleBackColor = true;
+            this.vdown.Click += new System.EventHandler(this.vdown_Click);
+            // 
+            // vup
+            // 
+            this.vup.Location = new System.Drawing.Point(527, 406);
+            this.vup.Name = "vup";
+            this.vup.Size = new System.Drawing.Size(79, 30);
+            this.vup.TabIndex = 25;
+            this.vup.Text = "+";
+            this.vup.UseVisualStyleBackColor = true;
+            this.vup.Click += new System.EventHandler(this.vup_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(474, 414);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 15);
+            this.label1.TabIndex = 24;
+            this.label1.Text = "speed";
+            // 
+            // pause
+            // 
+            this.pause.Location = new System.Drawing.Point(116, 17);
+            this.pause.Name = "pause";
+            this.pause.Size = new System.Drawing.Size(91, 32);
+            this.pause.TabIndex = 28;
+            this.pause.Text = "pause";
+            this.pause.UseVisualStyleBackColor = true;
+            this.pause.Click += new System.EventHandler(this.pause_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(994, 586);
+            this.Controls.Add(this.pause);
+            this.Controls.Add(this.vs);
+            this.Controls.Add(this.vdown);
+            this.Controls.Add(this.vup);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.J6BOX);
+            this.Controls.Add(this.J5BOX);
+            this.Controls.Add(this.J4BOX);
+            this.Controls.Add(this.J3BOX);
+            this.Controls.Add(this.J2BOX);
+            this.Controls.Add(this.J1BOX);
+            this.Controls.Add(this.J6DOWN);
+            this.Controls.Add(this.J6UP);
+            this.Controls.Add(this.J5DOWN);
+            this.Controls.Add(this.J5UP);
+            this.Controls.Add(this.J4DOWN);
+            this.Controls.Add(this.J4UP);
+            this.Controls.Add(this.J3DOWN);
+            this.Controls.Add(this.J3UP);
+            this.Controls.Add(this.J2DOWN);
+            this.Controls.Add(this.J2UP);
+            this.Controls.Add(this.J1DOWN);
+            this.Controls.Add(this.J1UP);
+            this.Controls.Add(this.J6);
+            this.Controls.Add(this.J5);
+            this.Controls.Add(this.J4);
+            this.Controls.Add(this.J3);
+            this.Controls.Add(this.J2);
+            this.Controls.Add(this.J1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label J1;
+        private System.Windows.Forms.Label J2;
+        private System.Windows.Forms.Label J3;
+        private System.Windows.Forms.Label J4;
+        private System.Windows.Forms.Label J5;
+        private System.Windows.Forms.Label J6;
+        private System.Windows.Forms.Button J1UP;
+        private System.Windows.Forms.Button J1DOWN;
+        private System.Windows.Forms.Button J2DOWN;
+        private System.Windows.Forms.Button J2UP;
+        private System.Windows.Forms.Button J3DOWN;
+        private System.Windows.Forms.Button J3UP;
+        private System.Windows.Forms.Button J4DOWN;
+        private System.Windows.Forms.Button J4UP;
+        private System.Windows.Forms.Button J5DOWN;
+        private System.Windows.Forms.Button J5UP;
+        private System.Windows.Forms.Button J6DOWN;
+        private System.Windows.Forms.Button J6UP;
+        private System.Windows.Forms.TextBox J1BOX;
+        private System.Windows.Forms.TextBox J2BOX;
+        private System.Windows.Forms.TextBox J3BOX;
+        private System.Windows.Forms.TextBox J4BOX;
+        private System.Windows.Forms.TextBox J5BOX;
+        private System.Windows.Forms.TextBox J6BOX;
+        private System.Windows.Forms.TextBox vs;
+        private System.Windows.Forms.Button vdown;
+        private System.Windows.Forms.Button vup;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button pause;
+    }
+}
+
---
+++ aeRobot/Form1.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.IO.Ports;
+
+namespace aeRobot
+{
+    public partial class Form1 : Form
+    {
+        SerialPort port;
+        public Form1()
+        {
+            InitializeComponent();
+            //串口初始化
+            this.FormClosed += new FormClosedEventHandler(Form1_FormClosed);
+
+           
+            if (port == null)
+            {
+               //COM1为Arduino使用的串口号，需根据实际情况调整
+                port = new SerialPort("COM1", 9600);
+                port.Open();
+            }
+        }
+        void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (port != null && port.IsOpen)
+            {
+                port.Close();
+            }
+        }   
+        private void PortWrite(string message)
+        {
+            if (port != null && port.IsOpen)
+            {
+                port.Write(message);
+            }
+        }
+       //private string  PortRead()
+       // {
+       //     if (port != null && port.IsOpen)
+       //     {
+       //        byte[] rec = new byte[1024 * 1024];
+       //        int r = port.Read(rec, 0, rec.Length);
+       //        return   System.Text.Encoding.Default.GetString(rec);  
+       //     }
+       //     else
+       //     {
+       //         return "读取失败！";
+       //     } 
+       // }
+        private void Add(int number)
+        {
+            switch (number)
+            {
+                case 1: PortWrite("1ups"); break;
+                case 2: PortWrite("2ups"); break;
+                case 3: PortWrite("3ups"); break;
+                case 4: PortWrite("4ups"); break;
+                case 5: PortWrite("5ups"); break;
+                case 6: PortWrite("6ups"); break;
+                case 7:PortWrite("vus");break;
+                default: PortWrite("ps"); break;
+            }
+
+        }
+        private void Sub(int number)
+        {
+            switch (number)
+            {
+                case 1: PortWrite("1ds"); break;
+                case 2: PortWrite("2ds"); break;
+                case 3: PortWrite("3ds"); break;
+                case 4: PortWrite("4ds"); break;
+                case 5: PortWrite("5ds"); break;
+                case 6: PortWrite("6ds"); break;
+                case 7:PortWrite("vds");break;
+                default: PortWrite("ps"); break;
+            }
+        }
+
+        #region 运动
+        double d = 5;
+        double v = 2;
+        private void J1UP_Click(object sender, EventArgs e)
+        {
+            double j1box= (double.Parse(J1BOX.Text) + d);
+            J1BOX.Text = Convert.ToString(j1box)+".0000000";
+            Add(1);
+            
+        }     
+        private void J1DOWN_Click(object sender, EventArgs e)
+        {
+            double jbox = (double.Parse(J1BOX.Text) - d);
+            J1BOX.Text = Convert.ToString(jbox) + ".0000000";
+            Sub(1);
+        }
+        private void J2UP_Click(object sender, EventArgs e)
+        {
+            double jbox = (double.Parse(J2BOX.Text) + d);
+            J2BOX.Text = Convert.ToString(jbox) + ".0000000";
+            Add(2);
+        }
+        private void J2DOWN_Click(object sender, EventArgs e)
+        {
+            double jbox = (double.Parse(J2BOX.Text) - d);
+            J2BOX.Text = Convert.ToString(jbox) + ".0000000";
+            Sub(2);
+        }
+        private void J3UP_Click(object sender, EventArgs e)
+        {
+            double jbox = (double.Parse(J3BOX.Text) +d);
+            J3BOX.Text = Convert.ToString(jbox) + ".0000000";
+            Add(3);
+        }
+        private void J3DOWN_Click(object sender, EventArgs e)
+        {
+            double jbox = (double.Parse(J3BOX.Text) -d);
+            J3BOX.Text = Convert.ToString(jbox) + ".0000000";
+            Sub(3);
+        }
+        private void J4UP_Click(object sender, EventArgs e)
+        {
+            double jbox = (double.Parse(J4BOX.Text) + d);
+            J4BOX.Text = Convert.ToString(jbox) + ".0000000";
+            Add(4);
+        }
+        private void J4DOWN_Click(object sender, EventArgs e)
+        {
+            double jbox = (double.Parse(J4BOX.Text) -d);
+            J4BOX.Text = Convert.ToString(jbox) + ".0000000";
+            Sub(4);
+        }
+        private void J5UP_Click(object sender, EventArgs e)
+        {
+            double jbox = (double.Parse(J5BOX.Text) + d);
+            J5BOX.Text = Convert.ToString(jbox) + ".0000000";
+            Add(5);
+        }
+        private void J5DOWN_Click(object sender, EventArgs e)
+        {
+            double jbox = (double.Parse(J5BOX.Text) - d);
+            J5BOX.Text = Convert.ToString(jbox) + ".0000000";
+            Sub(5);
+        }
+        private void J6UP_Click(object sender, EventArgs e)
+        {
+            double jbox = (double.Parse(J6BOX.Text) + d);
+            J6BOX.Text = Convert.ToString(jbox) + ".0000000";
+            Add(6);
+        }
+
+        private void J6DOWN_Click(object sender, EventArgs e)
+        {
+            double jbox = (double.Parse(J6BOX.Text) + d);
+            J6BOX.Text = Convert.ToString(jbox) + ".0000000";
+            Sub(6);
+        }
+
+        private void vup_Click(object sender, EventArgs e)
+        {
+            double jbox = (double.Parse(vs.Text) + v);
+            vs.Text = Convert.ToString(jbox) + ".0000000";
+            Add(7);
+        }
+
+        private void vdown_Click(object sender, EventArgs e)
+        {
+            double jbox = (double.Parse(vs.Text) - v);
+            vs.Text = Convert.ToString(jbox) + ".0000000";
+            Sub(7);
+        }
+
+        private void pause_Click(object sender, EventArgs e)
+        {
+            PortWrite("ps");
+            J1BOX.Text = "0.000000";
+            J2BOX.Text = "0.000000";
+            J3BOX.Text = "0.000000";
+            J4BOX.Text = "0.000000";
+            J5BOX.Text = "0.000000";
+            J6BOX.Text = "0.000000";
+            vs.Text = "5.000000";
+        }
+        #endregion
+
+    }
+}
